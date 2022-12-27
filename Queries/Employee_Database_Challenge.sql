bd1ca1064909e1bb41a5293b2b9ac89e883aaf93
Roselyanne Cepero Santos <roselyannecepero@gmail.com>
2022-12-27 16:20:04 -0500

    Removing duplicate rows

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -10,3 +10,12 @@ FROM employees as e
 	INNER JOIN titles as ti
 	ON e.emp_no = ti.emp_no
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+
+-- Use Dictinct with Orderby to remove duplicate rows, creating unique_titles table
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
